240ac279bb0a763cc47c462884cbbe70859172dc
Ayush.Vaibhav3 <ayush.vaibhav3@cognizant.com>
2023-11-10 12:26:06 +0530

    router implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlartsComponent } from './product-alarts/product-alarts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   imports: [
@@ -14,13 +15,15 @@ import { ProductAlartsComponent } from './product-alarts/product-alarts.componen
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      {path: 'products/:productId',component:ProductDetailsComponent},
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlartsComponent
+    ProductAlartsComponent,
+    ProductDetailsComponent
   ],
   bootstrap: [
     AppComponent
